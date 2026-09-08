a41360b9077cd286b4585076be618496279da6a7
Lenovo <manalip793@gmail.com>
2026-09-08 13:08:05 +0530

    dashboard_summary api, menu and splash screen

--- lib/routes/route_manager.dart
+++ lib/routes/route_manager.dart
@@ -222,12 +222,18 @@ class RouteManager {
     Get.to(() => const HandoverBagView(), transition: Transition.circularReveal, duration: const Duration(milliseconds: 200));
   }
 
-/// [isCollectionTrue] opens the queue in "collection mode" (the bag
+  /// [isCollectionTrue] opens the queue in "collection mode" (the bag
   /// registration dashboard's Collect action): only orders whose patients
   /// have already Arrived are listed, since a bag can only collect samples
   /// for patients that are physically present.
   static void navigateToPatientQueue({bool isCollectionTrue = false}) {
-    Get.to(() => const PatientQueueView(), binding: PatientQueueBinding(),arguments: {'isCollectionMode': isCollectionTrue}, transition: Transition.circularReveal, duration: const Duration(milliseconds: 200));
+    Get.to(
+      () => const PatientQueueView(),
+      binding: PatientQueueBinding(),
+      arguments: {'isCollectionMode': isCollectionTrue},
+      transition: Transition.circularReveal,
+      duration: const Duration(milliseconds: 200),
+    );
   }
 
   static void navigateToSampleCollection(AssignedPatient patient) {
